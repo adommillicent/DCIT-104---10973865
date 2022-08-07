cbccaf46be38de9f0221417ab8049f132e736ec8
Essilfie Prince Bondzie <essilfieprinceb2301@gmail.com>
2022-08-07 22:05:09 +0000

    average of prime numbers below a given number

---
+++ assgn.py.py
@@ -0,0 +1,28 @@
+Val=int(input("Enter a number: "))
+def prime_num():
+    num_of_prime=0
+    sum=0
+    for a in range(2,Val):
+            for i in range (2,a):        
+                if a%i==0:
+                    break
+            else:
+                print(a)
+                num_of_prime+=1
+                sum+=a
+    average=sum/num_of_prime
+    
+    print(f"Prime numbers below {Val} are",num_of_prime)
+    
+    print(f"The sum of the prime numbers below {Val} is",sum)
+    
+    print(f"The average of the prime numbers below {Val} is",average)
+    
+prime_num()
+    
+        
+    
+                
+        
+                
+                
